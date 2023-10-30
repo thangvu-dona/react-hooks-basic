a3e2d763d2f986476658d267be0effdeb326a6c8
Thang Vu <thangvu.dona@gmail.com>
2023-10-31 02:39:46 +0700

    Todo List - UI
    - render Todo list.
    - remove todo when click on it.
    - App-states: todoList, onTodoClick
    - TodoList -onclick -> call upper to parent for handling event.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todoList, setTodoList] = useState(() => {
+    const initTodoList = [
+      {id: 1, title: 'I love Easy Frontend! 😍'},
+      {id: 2, title: 'We love Easy Frontend! 🥰'},
+      {id: 3, title: 'They love Easy Frontend! 🐱‍🏍'},
+    ];
+
+    return initTodoList;
+  });
+
+  function handleTodoClick(todo) {
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
-      <h1>Welcome to React Hooks!</h1>
+      <h1>React Hooks - Todo List</h1>
 
-      <ColorBox />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
