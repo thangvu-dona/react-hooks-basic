36c3560d6b20d6203c28e61dd9d550a3077339bc
Thang Vu <thangvu.dona@gmail.com>
2023-10-31 04:28:20 +0700

    Post List UI - useEffect hook
    - fetch post list from API
    - render post list to UI
    - add useEffect(callback, depends) when fetch data from API.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
+import PostList from './components/PostList';
 
 function App() {
   const [todoList, setTodoList] = useState(() => {
     const initTodoList = [
-      {id: 1, title: 'I love Easy Frontend! 😍'},
-      {id: 2, title: 'We love Easy Frontend! 🥰'},
-      {id: 3, title: 'They love Easy Frontend! 🐱‍🏍'},
+      { id: 1, title: 'I love Easy Frontend! 😍' },
+      { id: 2, title: 'We love Easy Frontend! 🥰' },
+      { id: 3, title: 'They love Easy Frontend! 🐱‍🏍' },
     ];
 
     return initTodoList;
+  });
+
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const requestUrl = 'https://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+        console.log('responseJSON', responseJSON);
+
+        const { data } = responseJSON;
+        setPostList(data);
+      } catch (error) {
+        console.log('Failed to fetch post list');
+      }
+    }
+
+    console.log('POST list effect');
+    fetchPostList();
+  }, []);
+
+  useEffect(() => {
+    console.log('TODO list effect');
   });
 
   function handleTodoClick(todo) {
@@ -36,10 +62,11 @@ function App() {
 
   return (
     <div className="app">
-      <h1>React Hooks - Todo List</h1>
+      <h1>React Hooks - Post List</h1>
 
-      <TodoForm onSubmit={handleTodoFormSubmit} />
-      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+      <PostList posts={postList} />
+      {/* <TodoForm onSubmit={handleTodoFormSubmit} />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
     </div>
   );
 }
